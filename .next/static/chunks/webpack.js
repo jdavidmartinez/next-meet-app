948032b9af7518e0e1d4adeef57e2f16d5da293f
DaviD D <jdavid.martinez@gmail.com>
2021-08-17 08:55:06 -0500

    Section 23 Video 238 - Introducing API Routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1dc1dbe42eebd20b301a";
+/******/ 	var hotCurrentHash = "e9a7a5bdf413adfbab7f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,19 @@ import MeetupList from "../components/meetups/MeetupList";
 const DUMMY_MEETUPS = [
   {
     id: "m1",
-    title: " A first Meetup",
+    title: "A First Meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Torre_del_Filarete_y_la_Fontana_di_Piazza_Castello.jpg/1280px-Torre_del_Filarete_y_la_Fontana_di_Piazza_Castello.jpg",
-    addres: "Some address in some City",
-    description: " this is a first meetup",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
     id: "m2",
-    title: " A Second Meetup",
+    title: "A Second Meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Altar_della_Patria_September_2015-1.jpg/1280px-Altar_della_Patria_September_2015-1.jpg",
-    addres: "Some address in some City",
-    description: " this is a first meetup",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
   },
 ];
 
@@ -27,9 +27,12 @@ function HomePage(props) {
 //   const req = context.req;
 //   const res = context.res;
 
-//   // fetchh data from an API
+//   // fetch data from an API
+
 //   return {
-//     props: { DUMMY_MEETUPS },
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
 //   };
 // }
 
@@ -42,4 +45,5 @@ export async function getStaticProps() {
     revalidate: 1,
   };
 }
+
 export default HomePage;
