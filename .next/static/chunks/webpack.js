58ffd8169b32c0c4d368ac49629afdc2cc6b0974
DaviD D <jdavid.martinez@gmail.com>
2021-08-23 18:03:25 -0500

    Section 23 - Video 341 - Gettin Data from DataBase

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "38a49f5dfdcfbdfd2189";
+/******/ 	var hotCurrentHash = "ecf10bb9f52041e230c4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
